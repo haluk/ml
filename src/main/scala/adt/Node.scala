32265cda8df9d11c0999240f1c96349d291a3df2
haluk <hlk.dogan@gmail.com>
2016-09-06 02:03:28 -0500

    Nary Tree

---
+++ src/main/scala/adt/Node.scala
@@ -0,0 +1,10 @@
+package adt
+
+/**
+  * Created by hd on 9/6/16.
+  */
+sealed abstract class Tree
+
+case class Node(children: List[Tree]) extends Tree
+case class Leaf[A](data: A) extends Tree
+case object EmptyLeaf extends Tree
